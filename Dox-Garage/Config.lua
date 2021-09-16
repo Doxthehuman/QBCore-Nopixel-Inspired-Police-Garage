858ef67a341a0a741b1716c9d0b6b87108a6f653
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-09-16 16:16:27 +0200

    Update Config.lua

--- Dox-Garage/Config.lua
+++ Dox-Garage/Config.lua
@@ -1,10 +1,9 @@
 Config = {}
 
-Config.Pedlocation = {
-    {Cords = vector3(441.41162, -974.6804, 24.699926), h = 172.08871}  -- NPC Spawn Location.
+Config.PedLocations = { -- NPC Spawn Location(s).
+    {coords = vector4(441.41162, -974.6804, 24.699926, 172.08871)},
 }
 
-
 -- Dox Shop https://discord.gg/wQBuB3U5Ym
 -- Dox Shop https://discord.gg/wQBuB3U5Ym
 -- Dox Shop https://discord.gg/wQBuB3U5Ym
@@ -13,4 +12,3 @@ Config.Pedlocation = {
 -- Dox Shop https://discord.gg/wQBuB3U5Ym
 -- Dox Shop https://discord.gg/wQBuB3U5Ym
 -- Dox Shop https://discord.gg/wQBuB3U5Ym
-
